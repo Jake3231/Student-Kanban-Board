631d2d3466be3dbfa34b2862dea519b4860ed5be
Jake <Jake3231@users.noreply.github.com>
2022-11-30 23:44:49 -0600

    Add more prototype stuff

--- StudentKanbanBoard/App.js
+++ StudentKanbanBoard/App.js
@@ -1,14 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomePage from './Homepage'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-    <HomePage/>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen options={{
+          headerLargeTitle: true,
+        }} name="Kanban Board" component={HomePage} />
+    </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+const Stack = createNativeStackNavigator();
 
 const styles = StyleSheet.create({
   container: {
